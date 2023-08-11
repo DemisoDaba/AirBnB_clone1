7f6fef1f3fb1e95ab69f7c4849c95bfac0957898
DemisoDaba <demo.nkmt1@gmail.com>
2023-08-11 03:17:31 -0700

    Add necessary files

---
+++ models/engine/file_storage.py
@@ -0,0 +1,103 @@
+#!/usr/bin/python3
+"""
+file_storage module
+"""
+import datetime
+import json
+import os
+
+
+class FileStorage:
+    """
+    FileStorage class
+    """
+
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """returns the dictionary __objects"""
+        return self.__objects
+
+    def new(self, obj):
+        """sets in __objects the obj with key <obj class name>.id"""
+        self.__objects["{}.{}"
+                       .format(type(obj).__name__, obj.id)] = obj
+
+    def save(self):
+        """serializes __objects to the JSON file (path: __file_path)"""
+        with open(self.__file_path, mode="w") as f:
+            dict_storage = {k: v.to_dict() for k, v in self.__objects.items()}
+            json.dump(dict_storage, f)
+
+    def classes(self):
+        """Returns a dictionary of valid classes and their references."""
+        from models.user import BaseModel
+        from models.user import User
+        from models.state import State
+        from models.city import City
+        from models.amenity import Amenity
+        from models.place import Place
+        from models.review import Review
+
+        classes = {"BaseModel": BaseModel,
+                   "User": User,
+                   "State": State,
+                   "City": City,
+                   "Amenity": Amenity,
+                   "Place": Place,
+                   "Review": Review}
+        return classes
+
+    def reload(self):
+        """
+        deserializes the JSON file to __objects
+        (only if the JSON file (__file_path) exists ; otherwise,
+        do nothing. If the file doesn’t exist, no exception should be raised)
+        """
+        if not os.path.isfile(FileStorage.__file_path):
+            return
+        with open(FileStorage.__file_path, "r", encoding="utf-8") as f:
+            obj_dict = json.load(f)
+            obj_dict = {k: self.classes()[v["__class__"]]
+                        (**v) for k, v in obj_dict.items()}
+            FileStorage.__objects = obj_dict
+
+    def attributes(self):
+        """Returns the valid attributes and their types for classname."""
+        attributes = {
+            "BaseModel":
+                     {"id": str,
+                      "created_at": datetime.datetime,
+                      "updated_at": datetime.datetime},
+            "User":
+                     {"email": str,
+                      "password": str,
+                      "first_name": str,
+                      "last_name": str},
+            "State":
+                     {"name": str},
+            "City":
+                     {"state_id": str,
+                      "name": str},
+            "Amenity":
+                     {"name": str},
+            "Place":
+                     {"city_id": str,
+                      "user_id": str,
+                      "name": str,
+                      "description": str,
+                      "number_rooms": int,
+                      "number_bathrooms": int,
+                      "max_guest": int,
+                      "price_by_night": int,
+                      "latitude": float,
+                      "longitude": float,
+                      "amenity_ids": list},
+            "Review":
+                      {"place_id": str,
+                      "user_id": str,
+                      "text": str}
+                     }
+        return attributes
+
